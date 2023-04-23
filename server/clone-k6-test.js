a56b34103967834a4824a30f58a5c06eab4cfb60
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-04-23 14:57:24 -0500

    Added Grafana and Prometheus to Docker containers for running tests against the server

--- server/clone-k6-test.js
+++ server/clone-k6-test.js
@@ -13,7 +13,7 @@ export let options = {
 };
 
 export default function cloneTest() {
-    let homepage = http.get('127.26.86.50:300/api/shoes/1');
+    let homepage = http.get('http://172.26.86.50:300/api/shoes/1');
 
     check(homepage, {
         'status is 200': (r) => r.status === 200,
@@ -43,5 +43,27 @@ export default function cloneTest() {
     });
 
     // Add to cart
-    let cartLink = 'https://127.26.86.50:3000/api/shoes/cart'
+    let cartLink = 'http://172.26.86.50:3000/api/shoes/cart';
+    let cartParams = cartLink + "?product_id" + allProducts[randomLink] + "&quantity=1";
+
+    let addToCart = http.post(cartParams);
+    console.log(cartParams);
+    console.log(addToCart.url);
+    console.log(addToCart.status);
+
+    // Checkout
+    let checkoutParams = "?email=someone%40example.com&street_address=9845+Villa+Valencia&zip_code=80933";
+    let checkoutLink = "http://172.26.86.50:3000/api/checkout" + checkoutParams;
+
+    let checkout = http.post(checkoutLink);
+
+    console.log(checkout.status);
+    // console.log(checkout.body);
+
+    check(checkout, {
+        'status is 200': (r) => r.status === 200,
+        'Order Complete Check': (r) => r.html('h3').text().includes('Your order is complete!'),
+    });
+
+    sleep(1)
 }
