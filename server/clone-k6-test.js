d4a67a2ed7989eeca54f24582500d5432ad21c29
kevin-foreman <kevin.o.foreman2@gmail.com>
2023-04-27 17:52:36 -0500

    Scaled testing, but broke server, will re-visit in the AM

--- server/clone-k6-test.js
+++ server/clone-k6-test.js
@@ -3,13 +3,25 @@ import { check, sleep } from 'k6';
 
 export const options = {
     duration: '1m',
-    vus: 50,
+    vus: 100,
+};
+
+function apiEndpoint1() {
+    let response = http.get('http://localhost:8000/photos');
+    console.log(`Endpoint 1 status: ${response.status}`);
+};
+
+function apiEndpoint2() {
+    let response = http.get('http://localhost:8000/api/shoes');
+    console.log(`Endpoint 2 status: ${response.status}`);
 };
 
 export default function firstTest() {
-    const url = 'http://localhost:8000/api/shoes';
-
-    http.get(url);
+    if (__VU % 2 === 0) {
+        apiEndpoint1();
+    } else {
+        apiEndpoint2();
+    }
     sleep(1)
 };
 
